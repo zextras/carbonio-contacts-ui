8954b04328eb70c3880bb1832b27ba77366412c1
Matteo Dal Zovo <zovomat@gmail.com>
2022-02-15 15:29:26 +0100

    chore: pushing current state

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+export const CONTACTS_ROUTE = 'contacts';
+export const CONTACTS_APP_ID = 'carbonio-contacts-ui';
