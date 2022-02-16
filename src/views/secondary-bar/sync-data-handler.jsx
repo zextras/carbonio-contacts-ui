ca6c053d88027048b4698e26409dd7d803af64fc
Matteo Dal Zovo <zovomat@gmail.com>
2022-02-16 18:43:46 +0100

    chore: pushing current state

--- src/views/secondary-bar/sync-data-handler.jsx
+++ src/views/secondary-bar/sync-data-handler.jsx
@@ -6,7 +6,7 @@
 import { useNotify, useRefresh, store } from '@zextras/carbonio-shell-ui';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { isEmpty, map, reduce } from 'lodash';
+import { isEmpty, map, reduce, forEach, sortBy } from 'lodash';
 import { getContacts } from '../../store/actions/get-contacts';
 import { handleContactsSync } from '../../store/slices/contacts-slice';
 import { handleFoldersSync, handleRefresh } from '../../store/slices/folders-slice';
@@ -25,7 +25,8 @@ function getCnIds(contacts) {
 }
 
 export const SyncDataHandler = () => {
-	const notify = useNotify();
+	const notifyList = useNotify();
+	const [seq, setSeq] = useState(-1);
 	const refresh = useRefresh();
 	const dispatch = useDispatch();
 	const [initialized, setInitialized] = useState(false);
@@ -38,27 +39,35 @@ export const SyncDataHandler = () => {
 		}
 	}, [dispatch, initialized, refresh]);
 	useEffect(() => {
-		if (!isEmpty(notify) && initialized) {
-			if (notify.created?.cn) {
-				dispatch(getContacts(map(notify.created.cn, (c) => c.id)));
-			}
-			if (notify.modified?.cn) {
-				dispatch(getContacts(map(notify.modified.cn, (c) => c.id)));
-			}
-			if (notify.deleted?.length > 0) {
-				dispatch(handleContactsSync(notify));
-			}
-			if (
-				notify.deleted?.length > 0 ||
-				notify.modified?.folder ||
-				notify.created?.folder ||
-				notify.modified?.link ||
-				notify.created?.link
-			) {
-				dispatch(handleFoldersSync(notify));
+		if (initialized) {
+			if (notifyList.length > 0) {
+				forEach(sortBy(notifyList, 'seq'), (notify) => {
+					if (!isEmpty(notify) && notify.seq > seq) {
+						if (notify.created?.cn) {
+							dispatch(getContacts(map(notify.created.cn, (c) => c.id)));
+						}
+						if (notify.modified?.cn) {
+							dispatch(getContacts(map(notify.modified.cn, (c) => c.id)));
+						}
+						if (notify.deleted?.length > 0) {
+							dispatch(handleContactsSync(notify));
+						}
+						if (
+							notify.deleted?.length > 0 ||
+							notify.modified?.folder ||
+							notify.created?.folder ||
+							notify.modified?.link ||
+							notify.created?.link
+						) {
+							dispatch(handleFoldersSync(notify));
+						}
+
+						setSeq(notify.seq);
+					}
+				});
 			}
 		}
-	}, [dispatch, initialized, notify]);
+	}, [dispatch, initialized, notifyList, seq]);
 
 	return null;
 };
