00536d8fd2f264e2c49e3cfcc8180b4d9beebccf
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2022-04-04 17:36:49 +0200

    fix: fixed email validation

--- src/integrations/contact-input.jsx
+++ src/integrations/contact-input.jsx
@@ -13,10 +13,6 @@ import styled from 'styled-components';
 import moment from 'moment';
 import { useTranslation } from 'react-i18next';
 
-const emailRegex =
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars, max-len, no-control-regex
-	/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
 const getChipLabel = (contact) => {
 	if (contact.firstName ?? contact.middleName ?? contact.lastName) {
 		return trim(`${contact.firstName ?? ''} ${contact.middleName ?? ''} ${contact.lastName ?? ''}`);
@@ -168,12 +164,7 @@ export default function ContactInput({
 							includeGal: 1,
 							name: e.textContent
 						})
-							.then(({ match }) =>
-								map(match, (m) => ({
-									...m,
-									email: emailRegex.exec(m.email)[0]
-								}))
-							)
+							.then(({ match }) => filter(match, (m) => typeof m.email === 'string'))
 							.then((remoteResults) => {
 								const normRemoteResults = reduce(
 									remoteResults,
@@ -230,7 +221,7 @@ export default function ContactInput({
 		[allContacts]
 	);
 
-	const isValidEmail = useCallback((email) => emailRegex.test(email), []);
+	const isValidEmail = useCallback((email) => typeof email === 'string', []);
 
 	const editChip = useCallback((text, id) => {
 		setIdToRemove(id);
