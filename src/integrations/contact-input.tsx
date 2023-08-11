7b747372e701699f6e7e34192cbd43dbf2b5031f
Gammax92 <gottardifrancesco92@gmail.com>
2023-08-11 15:02:26 +0200

    feat: allow groups selection inside the contact-input

--- src/integrations/contact-input.tsx
+++ src/integrations/contact-input.tsx
@@ -3,6 +3,8 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import React, { useCallback, useEffect, useRef, useState, ReactElement, FC } from 'react';
+
 import { createSelector } from '@reduxjs/toolkit';
 import {
 	Avatar,
@@ -11,30 +13,38 @@ import {
 	Container,
 	Row,
 	Text,
-	Tooltip
+	Tooltip,
+	ChipProps,
+	ChipItem
 } from '@zextras/carbonio-design-system';
 import { soapFetch } from '@zextras/carbonio-shell-ui';
-import { filter, find, findIndex, map, reduce, some, startsWith, trim } from 'lodash';
+import { filter, find, findIndex, map, reduce, some, startsWith, trim, forEach } from 'lodash';
 import moment from 'moment';
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
+import styled, { DefaultTheme } from 'styled-components';
+
 import { useAppSelector } from '../hooks/redux';
 import { StoreProvider } from '../store/redux';
+import { Contact, Group } from '../types/contact';
+import { ContactsSlice, State } from '../types/store';
 
 const emailRegex =
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars, max-len, no-control-regex
 	/[^\s@]+@[^\s@]+\.[^\s@]+/;
 
-const getChipLabel = (contact) => {
-	if (contact.firstName ?? contact.middleName ?? contact.lastName) {
+function isGroup(contact: Contact | Group): contact is Group {
+	return (contact as Group).isGroup === true;
+}
+
+const getChipLabel = (contact: Contact | Group): string => {
+	if (!isGroup(contact)) {
 		return trim(`${contact.firstName ?? ''} ${contact.middleName ?? ''} ${contact.lastName ?? ''}`);
 	}
-	return contact.fullName ?? contact.email ?? contact.name ?? contact.address ?? '';
+	return contact?.display ?? '';
 };
 
-const Hint = ({ contact }) => {
-	const label = useMemo(() => contact.label ?? getChipLabel(contact), [contact]);
+const Hint = ({ contact }: { contact: Contact | Group }): ReactElement => {
+	const label = getChipLabel(contact);
 	return (
 		<Container
 			orientation="horizontal"
@@ -43,9 +53,9 @@ const Hint = ({ contact }) => {
 			minWidth="16rem"
 			minHeight="2rem"
 		>
-			<Avatar label={contact.label ?? getChipLabel(contact)} />
+			<Avatar label={label} />
 			<Container orientation="vertical" crossAlignment="flex-start" padding={{ left: 'small' }}>
-				{label !== contact.email ? (
+				{!isGroup(contact) ? (
 					<>
 						<Row takeAvailableSpace mainAlignment="flex-start">
 							<Text size="large">{label}</Text>
@@ -62,18 +72,24 @@ const Hint = ({ contact }) => {
 	);
 };
 
-const SkeletonTile = styled.div`
-	width: ${({ width }) => width ?? '1rem'};
-	max-width: ${({ width }) => width ?? '1rem'};
-	min-width: ${({ width }) => width ?? '1rem'};
-	height: ${({ height }) => height ?? '1rem'};
-	max-height: ${({ height }) => height ?? '1rem'};
-	min-height: ${({ height }) => height ?? '1rem'};
-	border-radius: ${({ radius }) => radius ?? '0.125rem'};
-	background: ${({ theme }) => theme.palette.gray2.regular};
-`;
+interface SkeletonTileProps {
+	width?: string;
+	height?: string;
+	radius?: string;
+	theme: DefaultTheme;
+}
 
-const Loader = () => (
+const SkeletonTile = styled.div<SkeletonTileProps>`
+	width: ${({ width }): string => width ?? '1rem'};
+	max-width: ${({ width }): string => width ?? '1rem'};
+	min-width: ${({ width }): string => width ?? '1rem'};
+	height: ${({ height }): string => height ?? '1rem'};
+	max-height: ${({ height }): string => height ?? '1rem'};
+	min-height: ${({ height }): string => height ?? '1rem'};
+	border-radius: ${({ radius }): string => radius ?? '0.125rem'};
+	background: ${({ theme }): string => theme.palette.gray2.regular};
+`;
+const Loader = (): ReactElement => (
 	<Container
 		orientation="horizontal"
 		mainAlignment="flex-start"
@@ -94,35 +110,47 @@ const Loader = () => (
 	</Container>
 );
 
-function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5', ...props }) {
-	const [defaults, setDefaults] = useState([]);
-	const [options, setOptions] = useState([]);
+type ContactInput = {
+	onChange?: (items: ChipItem<string | Contact>[]) => void;
+	defaultValue: Array<Contact>;
+	placeholder: string;
+	background?: keyof DefaultTheme['palette'];
+};
+const ContactInput: FC<ContactInput> = ({
+	onChange,
+	defaultValue,
+	placeholder,
+	background = 'gray5',
+	...props
+}) => {
+	const [defaults, setDefaults] = useState<Array<Contact>>([]);
+	const [options, setOptions] = useState<any>([]);
 	const [idToRemove, setIdToRemove] = useState('');
 	const [t] = useTranslation();
-	const inputRef = useRef();
+	const inputRef = useRef<HTMLInputElement | null>(null);
 
 	useEffect(() => {
 		setDefaults(
 			map(filter(defaultValue, (c) => c.id !== idToRemove) ?? [], (obj) => ({
 				...obj,
-				label: obj.label ?? getChipLabel(obj)
+				label: getChipLabel(obj)
 			}))
 		);
 	}, [defaultValue, idToRemove]);
 
-	const allContacts = useAppSelector(
+	const allContacts: Array<Contact> = useAppSelector(
 		createSelector(
-			(state) => state.contacts.contacts,
-			(contacts) =>
+			(state: State) => state.contacts.contacts,
+			(contacts: ContactsSlice['contacts']) =>
 				reduce(
 					contacts,
-					(acc, folderContacts) =>
+					(acc: Array<Contact>, folderContacts) =>
 						folderContacts
 							? [
 									...acc,
 									...reduce(
 										folderContacts,
-										(acc2, contact) => [
+										(acc2: any, contact) => [
 											...acc2,
 											...map(contact.email, (email) => ({
 												...contact,
@@ -138,6 +166,7 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 				)
 		)
 	);
+
 	const isValidEmail = useCallback((email) => emailRegex.test(email), []);
 
 	const editChip = useCallback((text, id) => {
@@ -152,18 +181,17 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 				if (inputRef?.current) {
 					inputRef.current.innerText = inputRef.current.innerText.replaceAll('\n', '');
 				}
-				if (options.length > 0 && !find(options, { id: 'loading' })) {
-					onChange([...defaults, { ...options[0]?.value }]);
+				if (options?.length > 0 && !find(options, { id: 'loading' })) {
+					onChange && onChange([...defaults, { ...(options[0]?.value as Contact) }]);
 					if (inputRef?.current) {
 						inputRef.current.innerText = '';
 					}
 					setOptions([]);
 					return;
 				}
-				const valueToAdd = inputRef.current.innerText.replaceAll('\n', '');
-				const id = moment().valueOf();
-				const chip = {
-					email: valueToAdd,
+				const valueToAdd = inputRef?.current?.innerText.replaceAll('\n', '');
+				const id = moment().valueOf().toString();
+				const chip: ChipProps = {
 					id,
 					label: valueToAdd,
 					error: !isValidEmail(valueToAdd),
@@ -183,7 +211,7 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 					chip.avatarIcon = 'AlertCircleOutline';
 				}
 				if (valueToAdd !== '') {
-					onChange([...defaults, { ...chip }]);
+					onChange && onChange([...defaults, { ...chip }] as ChipItem<string | Contact>[]);
 				}
 				if (inputRef?.current) {
 					inputRef.current.innerText = '';
@@ -207,11 +235,11 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 								)
 							)
 						);
-					} catch (err) {
+					} catch (err: any) {
 						reject(new Error(err));
 					}
 				})
-					.then((localResults) => {
+					.then((localResults: any) => {
 						if (localResults.length > 0) {
 							setOptions(localResults);
 						}
@@ -220,16 +248,16 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 							includeGal: 1,
 							name: e.textContent
 						})
-							.then(({ match }) =>
-								map(match, (m) => ({
+							.then((autoCompleteResult: any) =>
+								map(autoCompleteResult.match, (m) => ({
 									...m,
-									email: emailRegex.exec(m.email)[0]?.slice(1, -1)
+									email: m.isGroup ? m.display : emailRegex.exec(m.email)?.[0]?.slice(1, -1)
 								}))
 							)
-							.then((remoteResults) => {
+							.then((remoteResults: any) => {
 								const normRemoteResults = reduce(
 									remoteResults,
-									(acc, result) => {
+									(acc, result: any) => {
 										const localIndex = findIndex(acc, ['email', result.email]);
 										if (localIndex >= 0) {
 											return acc;
@@ -241,7 +269,13 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 												firstName: result.first,
 												lastName: result.last,
 												company: result.company,
-												fullName: result.full
+												fullName: result.full,
+												display: result.display,
+												isGroup: result.isGroup,
+												// check if needed
+												id: result.id,
+												l: result.l,
+												exp: result.exp
 											}
 										];
 									},
@@ -249,21 +283,26 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 								);
 								setOptions(
 									map(
-										filter(normRemoteResults, (c) =>
-											some([c.firstName, c.lastName, c.company, c.email, c.fullName], (field) =>
-												startsWith(field?.toLowerCase().trim(), e.textContent.toLowerCase())
+										filter(normRemoteResults, (c: any) =>
+											some(
+												// eslint-disable-next-line max-len
+												[c?.firstName, c?.lastName, c?.company, c?.email, c?.fullName, c?.display],
+												(field: any) =>
+													startsWith(field?.toLowerCase().trim(), e.textContent.toLowerCase())
 											)
 										),
-										(contact) => ({
-											label: contact.label ?? getChipLabel(contact),
+										(contact: any) => ({
+											label: contact?.label ?? getChipLabel(contact),
 											value: {
 												id: `${contact.id} ${contact.email}`,
 												email: contact?.email,
-												firstName: contact.firstName,
-												lastName: contact.lastName,
-												company: contact.company,
-												fullName: contact.fullName,
-												label: contact.label ?? getChipLabel(contact)
+												firstName: contact?.firstName,
+												lastName: contact?.lastName,
+												company: contact?.company,
+												fullName: contact?.fullName,
+												isGroup: contact?.isGroup,
+												groupId: contact?.id,
+												label: contact?.label ?? getChipLabel(contact)
 											},
 											customComponent: <Hint contact={contact} />
 										})
@@ -285,9 +324,8 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 	const onAdd = useCallback(
 		(valueToAdd) => {
 			if (typeof valueToAdd === 'string') {
-				const id = moment().valueOf();
-				const chip = {
-					email: valueToAdd,
+				const id = moment().valueOf().toString();
+				const chip: ChipProps = {
 					id,
 					label: valueToAdd,
 					error: !isValidEmail(valueToAdd),
@@ -308,23 +346,43 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 				}
 				return chip;
 			}
+			if (valueToAdd.isGroup) {
+				const groupChips: Array<any> = [];
+				soapFetch('GetContacts', {
+					_jsns: 'urn:zimbraMail',
+					cn: {
+						id: valueToAdd.groupId
+					},
+					derefGroupMember: true
+				}).then((result: any) => {
+					const id = moment().valueOf().toString();
+					const members = result && result?.cn && result?.cn[0].m;
+					forEach(members, (member, i) => {
+						groupChips.push({
+							email: member.cn[0]._attrs.email,
+							id,
+							label: member.cn[0]._attrs.email,
+							error: !isValidEmail(member.cn[0]._attrs.email),
+							actions: [
+								{
+									id: 'action1',
+									label: isValidEmail(member.cn[0]._attrs.email)
+										? t('label.edit_email', 'Edit E-mail')
+										: t('label.edit_invalid_email', 'E-mail is invalid, click to edit it'),
+									icon: 'EditOutline',
+									type: 'button',
+									onClick: () => editChip(member.cn[0]._attrs.email, id)
+								}
+							]
+						});
+					});
+					setDefaults((prevValue) => [...prevValue, ...groupChips]);
+				});
+				return {};
+			}
 			return valueToAdd;
 		},
 		[editChip, isValidEmail, t]
-	);
-
-	const customChip = (chipProps) => (
-		<Chip
-			{...chipProps}
-			avatarLabel={chipProps.label}
-			label={
-				<Tooltip label={chipProps.email ?? chipProps.address} maxWidth="unset">
-					<Row wrap="nowrap">
-						<Text size="extrasmall">{chipProps.label}</Text>
-					</Row>
-				</Tooltip>
-			}
-		/>
 	);
 
 	return (
@@ -343,16 +401,15 @@ function ContactInput({ onChange, defaultValue, placeholder, background = 'gray5
 				onAdd={onAdd}
 				requireUniqueChips
 				createChipOnPaste
-				ChipComponent={customChip}
 				pasteSeparators={[',', ' ', ';', '\n']}
 				separators={['NumpadEnter', ',']}
 				{...props}
 			/>
 		</Container>
 	);
-}
+};
 
-const ContactInputComp = (props) => (
+const ContactInputComp = (props: ContactInput): ReactElement => (
 	<StoreProvider>
 		<ContactInput {...props} />
 	</StoreProvider>
