2b0267a22bd6b340d0e0184999839881a99f1d24
Luca Stauble <luca.stauble@zextras.com>
2024-04-04 11:05:05 +0200

    test: add test for createMountpoints API function

--- src/network/api/create-mountpoint.test.ts
+++ src/network/api/create-mountpoint.test.ts
@@ -4,11 +4,17 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { faker } from '@faker-js/faker';
-import { ErrorSoapBodyResponse } from '@zextras/carbonio-shell-ui';
+import { ErrorSoapBodyResponse, FOLDERS } from '@zextras/carbonio-shell-ui';
+import { times } from 'lodash';
 
-import { createMountpoints } from './create-mountpoints';
+import {
+	createMountpoints,
+	CreateMountpointsRequest,
+	CreateMountpointsResponse
+} from './create-mountpoints';
 import { createAPIInterceptor } from '../../carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
 import { NAMESPACES } from '../../constants/api';
+import { ShareInfo } from '../../model/share-info';
 
 describe('createMountpoints', () => {
 	it('should raise an exception if the response contains a Fault', () => {
@@ -25,11 +31,36 @@ describe('createMountpoints', () => {
 	});
 
 	it('should call the API passing the correct static params', async () => {
-		const interceptor = createAPIInterceptor('Batch');
-		await createMountpoints();
+		const shares: Array<ShareInfo & { mountpointName: string }> = times(3, () => ({
+			folderId: `${faker.string.uuid()}:${faker.number.int({ min: 2 })}`,
+			folderPath: `/${faker.word.noun(1)}`,
+			folderUuid: faker.string.uuid(),
+			granteeType: 'grp',
+			ownerEmail: faker.internet.email(),
+			ownerId: faker.string.uuid(),
+			ownerName: faker.person.fullName(),
+			rights: 'r',
+			mountpointName: faker.word.noun(1)
+		}));
+		const interceptor = createAPIInterceptor<CreateMountpointsRequest, CreateMountpointsResponse>(
+			'Batch'
+		);
+		await createMountpoints(shares);
 		const request = await interceptor;
-		expect(request).toEqual({
-			_jsns: NAMESPACES.mail
+		expect(request.CreateMountpointRequest).toBeDefined();
+		shares.forEach((share) => {
+			expect(request.CreateMountpointRequest).toContainEqual(
+				expect.objectContaining({
+					link: expect.objectContaining({
+						l: FOLDERS.USER_ROOT,
+						name: share.mountpointName,
+						rid: share.folderId,
+						view: 'contact',
+						zid: share.ownerId
+					}),
+					_jsns: NAMESPACES.mail
+				})
+			);
 		});
 	});
 });
