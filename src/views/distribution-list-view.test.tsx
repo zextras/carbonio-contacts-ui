53bc53666303ec430d2cc71f1a9657263ebf9e9b
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-09-13 16:50:06 +0200

    test(distribution-list-view): remove irrelevant test

--- src/views/distribution-list-view.test.tsx
+++ src/views/distribution-list-view.test.tsx
@@ -5,40 +5,22 @@
  */
 import React from 'react';
 
-import { faker } from '@faker-js/faker';
 import { waitFor } from '@testing-library/react';
 
-import GroupsAppView from './distribution-list-view';
+import DistributionListAppView from './distribution-list-view';
 import { screen, setupTest } from '../carbonio-ui-commons/test/test-setup';
 import { ROUTES_INTERNAL_PARAMS } from '../constants';
-import {
-	createFindContactGroupsResponse,
-	registerFindContactGroupsHandler
-} from '../tests/msw-handlers/find-contact-groups';
 import { registerGetAccountDistributionListsHandler } from '../tests/msw-handlers/get-account-distribution-lists';
-import { createCnItem, generateDistributionList } from '../tests/utils';
+import { generateDistributionList } from '../tests/utils';
 
 describe('App view', () => {
-	it.each([/* '/' */ '/groups'])('should render groups on %s', async () => {
-		const cgName = faker.word.words();
-		const handler = registerFindContactGroupsHandler({
-			offset: 0,
-			findContactGroupsResponse: createFindContactGroupsResponse([createCnItem(cgName)])
-		});
-		setupTest(<GroupsAppView />, {
-			initialEntries: [`/${ROUTES_INTERNAL_PARAMS.route.contactGroups}`]
-		});
-		await waitFor(() => expect(handler).toHaveBeenCalled());
-		expect(await screen.findByText(cgName)).toBeVisible();
-	});
-
 	describe('Distribution lists', () => {
 		it.each([ROUTES_INTERNAL_PARAMS.filter.member, ROUTES_INTERNAL_PARAMS.filter.manager])(
 			'should render distribution lists on filter %s',
 			async (filter) => {
 				const dl = generateDistributionList({ isOwner: true, isMember: true });
 				const handler = registerGetAccountDistributionListsHandler([dl]);
-				setupTest(<GroupsAppView />, {
+				setupTest(<DistributionListAppView />, {
 					initialEntries: [`/${ROUTES_INTERNAL_PARAMS.route.distributionLists}/${filter}`]
 				});
 				await waitFor(() => expect(handler).toHaveBeenCalled());
@@ -51,7 +33,7 @@ describe('App view', () => {
 			async (nextPath) => {
 				const dl = generateDistributionList({ isMember: true });
 				const handler = registerGetAccountDistributionListsHandler([dl]);
-				setupTest(<GroupsAppView />, {
+				setupTest(<DistributionListAppView />, {
 					initialEntries: [`/${ROUTES_INTERNAL_PARAMS.route.distributionLists}${nextPath}`]
 				});
 				await waitFor(() => expect(handler).toHaveBeenCalled());
