26ca59ae4527d104bf333b7e45947937f0a6f1c1
Davide Frison <davide.frison@zextras.com>
2024-09-16 18:09:40 +0200

    test: add simple test for shared contact groups
    - not sure it is the correct query, let's see

---
+++ src/network/api/find-contact-groups.test.ts
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { FindContactGroupsSoapApiRequest, findUserContactGroups } from './find-contact-groups';
+import { createSoapAPIInterceptor } from '../../carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
+
+describe('Find Contact Groups API', () => {
+	it('should request contact groups of given account', async () => {
+		const interceptor = createSoapAPIInterceptor<FindContactGroupsSoapApiRequest>('Search');
+
+		await findUserContactGroups('123', 0);
+
+		const request = await interceptor;
+
+		expect(request.query).toBe('#type:group in:contacts accountId:123');
+	});
+});
