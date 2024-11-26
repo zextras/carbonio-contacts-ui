7e0a4549d74d023a1f206249b79f86d56b6049e5
Rodley <100950196+rodleyorosa@users.noreply.github.com>
2024-11-26 09:33:48 +0100

    refactor: replace Spinner of Shell with the DS one (#270)
    ---------
    
    Co-authored-by: Giuliano Caregnato <giuliano.caregnato@zextras.com>

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,6 @@ import {
 	registerFunctions,
 	SearchViewProps,
 	SecondaryBarComponentProps,
-	Spinner
 } from '@zextras/carbonio-shell-ui';
 import { useTranslation } from 'react-i18next';
 
@@ -39,6 +38,7 @@ import { ContactInputIntegrationWrapper } from './legacy/integrations/contact-in
 import createContactIntegration from './legacy/integrations/create-contact';
 import { StoreProvider } from './legacy/store/redux';
 import { SyncDataHandler } from './legacy/views/secondary-bar/sync-data-handler';
+import { Spinner } from "./components/Spinner";
 
 const LazyContactsView = lazy(
 	() => import(/* webpackChunkName: "contacts-view" */ './views/contacts-view')
