e9965bbfb63a77249872afbaa6c72ddb1ce93811
Luca Stauble <luca.stauble@gmail.com>
2024-12-12 10:57:57 +0100

    refactor: move files

--- src/app/tests/auth-guard.test.tsx
+++ src/app/tests/auth-guard.test.tsx
@@ -5,9 +5,9 @@
  */
 import React from 'react';
 
-import { AuthGuard } from './auth-guard';
-import { useAuthenticated } from '../__mocks__/@zextras/carbonio-shell-ui';
-import { setupTest, screen } from './carbonio-ui-commons/test/test-setup';
+import { useAuthenticated } from '../../../__mocks__/@zextras/carbonio-shell-ui';
+import { setupTest, screen } from '../../carbonio-ui-commons/test/test-setup';
+import { AuthGuard } from '../auth-guard';
 
 describe('AuthGuard', () => {
 	it('should render the child component when the user is authenticated', () => {
