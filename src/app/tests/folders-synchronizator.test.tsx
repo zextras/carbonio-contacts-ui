172d3775560059f7a15175abc59eb8161ef1ba0f
Luca Stauble <luca.stauble@gmail.com>
2024-12-12 17:00:14 +0100

    test: add spy for useInitializeFolders

--- src/app/tests/folders-synchronizator.test.tsx
+++ src/app/tests/folders-synchronizator.test.tsx
@@ -6,13 +6,16 @@
 import React from 'react';
 
 import { FOLDER_VIEW } from '../../carbonio-ui-commons/constants';
-import { useInitializeFolders } from '../../carbonio-ui-commons/hooks/use-initialize-folders';
+import * as commonsHook from '../../carbonio-ui-commons/hooks/use-initialize-folders';
 import { setupTest } from '../../carbonio-ui-commons/test/test-setup';
 import { FoldersSynchronizator } from '../folders-syncronization';
 
 describe('FoldersSynchronizator', () => {
 	it('should call the useInitializeFolders hook with the contact folder view', () => {
+		const useInitializeFoldersSpy = jest.spyOn(commonsHook, 'useInitializeFolders');
+
 		setupTest(<FoldersSynchronizator />);
-		expect(useInitializeFolders).toHaveBeenCalledWith(FOLDER_VIEW.contact);
+
+		expect(useInitializeFoldersSpy).toHaveBeenCalledWith(FOLDER_VIEW.contact);
 	});
 });
