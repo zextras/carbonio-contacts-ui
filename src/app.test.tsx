7f585ed9c9041a5d4ca7c97b2659530debd5410f
Luca Stauble <luca.stauble@zextras.com>
2024-12-24 11:09:32 +0100

    refactor: use upsert app to add translation
    Co-authored-by: rodleyorosa <rodley.orosa@zextras.com>

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -16,7 +16,7 @@ import {
 	createSoapAPIInterceptor
 } from './carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
 import { setupTest } from './carbonio-ui-commons/test/test-setup';
-import { CONTACT_BOARD_ID } from './constants';
+import { CONTACT_BOARD_ID, CONTACTS_APP_ID } from './constants';
 import { ContactInput } from './legacy/integrations/contact-input';
 
 // mocking the worker. in commons jest-setup the worker is already mocked, but is improperly defined with wrong types and
@@ -129,6 +129,15 @@ describe('App', () => {
 		expect(addBoardView).toHaveBeenCalledWith<Parameters<typeof shell.addBoardView>>({
 			id: 'edit-contact-group-board',
 			component: expect.anything()
+		});
+	});
+
+	it('should register upsertApp', () => {
+		const upsertApp = jest.spyOn(shell, 'upsertApp');
+		setupTest(<App />);
+		expect(upsertApp).toHaveBeenCalledWith<Parameters<typeof shell.upsertApp>>({
+			name: CONTACTS_APP_ID,
+			display: 'Contacts'
 		});
 	});
 
