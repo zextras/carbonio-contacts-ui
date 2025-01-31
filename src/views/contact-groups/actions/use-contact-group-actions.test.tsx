45ac863e6ad566c3f7ad93671e78aebf643f2a48
Davide Frison <davide.frison@zextras.com>
2025-01-31 19:43:56 +0100

    test: use-contact-group-actions.test.tsx add store to tests

--- src/views/contact-groups/actions/use-contact-group-actions.test.tsx
+++ src/views/contact-groups/actions/use-contact-group-actions.test.tsx
@@ -10,15 +10,17 @@ import * as shell from '@zextras/carbonio-shell-ui';
 import { useContactGroupActions } from './use-contact-group-actions';
 import { setupHook } from '../../../carbonio-ui-commons/test/test-setup';
 import { ACTION_IDS } from '../../../constants';
+import { generateStore } from '../../../legacy/tests/generators/store';
 import { buildContactGroup, buildMembers } from '../../../tests/model-builder';
 
 describe('useContactGroupActions', () => {
+	const store = generateStore();
 	it('should return send mail action when the contact group has at least 1 member', () => {
 		jest.spyOn(shell, 'useIntegratedFunction').mockReturnValue([jest.fn(), true]);
 		const contactGroup = buildContactGroup({
 			members: buildMembers(faker.number.int({ min: 1, max: 100 }))
 		});
-		const { result } = setupHook(() => useContactGroupActions()(contactGroup));
+		const { result } = setupHook(() => useContactGroupActions()(contactGroup), { store });
 
 		expect(result.current).toContainEqual({
 			id: ACTION_IDS.sendEmailCG,
@@ -30,7 +32,7 @@ describe('useContactGroupActions', () => {
 
 	it('should not return send mail action when the contact group has 0 members', () => {
 		const contactGroup = buildContactGroup();
-		const { result } = setupHook(() => useContactGroupActions()(contactGroup));
+		const { result } = setupHook(() => useContactGroupActions()(contactGroup), { store });
 		expect(result.current).not.toContainEqual({
 			id: ACTION_IDS.sendEmailCG,
 			label: 'Send e-mail',
@@ -41,7 +43,7 @@ describe('useContactGroupActions', () => {
 
 	it('should return delete action', () => {
 		const contactGroup = buildContactGroup();
-		const { result } = setupHook(() => useContactGroupActions()(contactGroup));
+		const { result } = setupHook(() => useContactGroupActions()(contactGroup), { store });
 
 		expect(result.current).toContainEqual({
 			id: ACTION_IDS.deleteCG,
