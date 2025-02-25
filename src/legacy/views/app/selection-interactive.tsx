64d88cf942d77f143fe1244c8481def4bcdd6f0b
rodleyorosa <rodley.orosa@zextras.com>
2025-02-25 14:14:51 +0100

    fix: fix wrong path

--- src/legacy/views/app/selection-interactive.tsx
+++ src/legacy/views/app/selection-interactive.tsx
@@ -15,7 +15,6 @@ import { selectAllContactsInFolder } from '../../store/selectors/contacts';
 export default function SelectionInteractive(): JSX.Element {
 	const [t] = useTranslation();
 	const { folderId } = useParams<{ folderId: string }>();
-	console.log('folderId: ', folderId);
 	const contacts = useAppSelector((state) => selectAllContactsInFolder(state, folderId!));
 	const trashMessages = useMemo(
 		() => [
