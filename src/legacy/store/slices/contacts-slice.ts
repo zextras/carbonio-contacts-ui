3186d9c8f2923d2e4e0b5476b62435e92036af4c
Davide Frison <davide.frison@zextras.com>
2025-03-17 16:25:06 +0100

    refactor: remove unused handle-contacts-sync.ts

--- src/legacy/store/slices/contacts-slice.ts
+++ src/legacy/store/slices/contacts-slice.ts
@@ -6,39 +6,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 import { INITIAL_STATE } from './constants';
-import { SEARCHED_FOLDER_STATE_STATUS } from '../../../constants';
-import { State } from '../../types/store';
 import { folderAction } from '../actions/folder-action';
 import { folderActionPending, folderActionRejected } from '../reducers/folder-action';
-import {
-	handleDeletedContactsSyncReducer,
-	handleCreatedContactsSyncReducer,
-	handleModifiedContactsSyncReducer,
-	handleResetContactsSyncReducer
-} from '../reducers/handle-contacts-sync';
 
 export const contactsSlice = createSlice({
 	name: 'contacts',
 	initialState: INITIAL_STATE,
-	reducers: {
-		handleModifiedContactsSync: handleModifiedContactsSyncReducer,
-		handleCreatedContactsSync: handleCreatedContactsSyncReducer,
-		handleDeletedContactsSync: handleDeletedContactsSyncReducer,
-		handleResetContactsSync: handleResetContactsSyncReducer
-	},
+	reducers: {},
 	extraReducers: (builder) => {
 		builder.addCase(folderAction.pending, folderActionPending);
 		builder.addCase(folderAction.rejected, folderActionRejected);
 	}
 });
 
-export const {
-	handleCreatedContactsSync,
-	handleModifiedContactsSync,
-	handleDeletedContactsSync,
-	handleResetContactsSync
-} = contactsSlice.actions;
 export const contactSliceReducer = contactsSlice.reducer;
-
-export const selectFolderHasMore = (state: State, id: string): boolean =>
-	state.contacts.searchedInFolder?.[id] === SEARCHED_FOLDER_STATE_STATUS.hasMore;
