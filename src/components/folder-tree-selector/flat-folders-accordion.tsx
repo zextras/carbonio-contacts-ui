29a50059dee65da451aeb6bb44149509f39674b0
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2025-04-07 17:01:03 +0200

    revert: "[CO-1917] move restore shared account (#327)" (#332)
    This reverts commit 3a191b97adc69bbe2c21b7d57a9759b8cf6f9f6e and PR #327.

--- src/components/folder-tree-selector/flat-folders-accordion.tsx
+++ src/components/folder-tree-selector/flat-folders-accordion.tsx
@@ -138,7 +138,6 @@ const FlatFoldersAccordionFolder: FC<FlatFoldersAccordionFolderProps> = ({
 			height={FOLDER_ROW_HEIGHT}
 			onClick={selectionHandler}
 			wrap="nowrap"
-			style={{ overflow: 'hidden' }}
 			{...rest}
 		>
 			<Row mainAlignment="flex-start" wrap="nowrap" width="fill">
