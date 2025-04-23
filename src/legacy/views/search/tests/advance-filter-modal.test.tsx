5fd6ee1ed65c84bed1e798786f266771469b7700
Davide Frison <davide.frison@zextras.com>
2025-04-23 12:59:23 +0200

    test: check search in advanced modal runs with the new keywords

--- src/legacy/views/search/tests/advance-filter-modal.test.tsx
+++ src/legacy/views/search/tests/advance-filter-modal.test.tsx
@@ -15,18 +15,17 @@ import { AdvancedFilterModal, AdvancedFilterModalProps } from '../advance-filter
 describe('Advanced filter modal', () => {
 	const tMock = ((key: string, _defaultValue?: any) => key) as TFunction<'translation'>;
 	const onSearchConfirmMock = jest.fn();
-	const mockedQuery = [
-		{
-			id: 'query1',
-			label: 'testKeyword1',
-			value: 'testKeyword1'
-		},
-		{
-			id: 'query2',
-			label: 'testKeyword2',
-			value: 'testKeyword2'
-		}
-	];
+	const firstQueryChip = {
+		id: 'query1',
+		label: 'testKeyword1',
+		value: 'testKeyword1'
+	};
+	const secondQueryChip = {
+		id: 'query2',
+		label: 'testKeyword2',
+		value: 'testKeyword2'
+	};
+	const mockedQuery = [firstQueryChip, secondQueryChip];
 	const properties: AdvancedFilterModalProps = {
 		open: true,
 		onClose: jest.fn(),
@@ -96,5 +95,22 @@ describe('Advanced filter modal', () => {
 		expect(chips[0]).toHaveAttribute('value', 'testKeyword1');
 		// eslint-disable-next-line
 		expect(chips[1]).toHaveAttribute('value', 'testKeyword2');
+	});
+	it('should run the search with the new keywords', async () => {
+		const { user } = setupTest(<AdvancedFilterModal {...properties} />);
+
+		const keywordsInput = await screen.findByRole('textbox', { name: /keywords/i });
+		await user.type(keywordsInput, 'MyNewKeyword');
+		await user.tab();
+		const searchButton = screen.getByRole('button', { name: /search/i });
+		await user.click(searchButton);
+		expect(onSearchConfirmMock).toHaveBeenCalledWith({
+			includeSharedFolders: false,
+			query: [
+				expect.objectContaining(firstQueryChip),
+				expect.objectContaining(secondQueryChip),
+				expect.objectContaining({ label: 'MyNewKeyword' })
+			]
+		});
 	});
 });
