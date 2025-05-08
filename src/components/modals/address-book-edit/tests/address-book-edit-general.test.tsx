b928af777f67f09ed1efbe4f405ff4bfc7910e14
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-05-08 10:56:11 +0200

    test(address-book-edit-general.test.tsx): add test for empty modal title
    Add a test case to verify the modal title renders correctly when the
    address book does not exist.

---
+++ src/components/modals/address-book-edit/tests/address-book-edit-general.test.tsx
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+
+import { setupTest } from '../../../../carbonio-ui-commons/test/test-setup';
+import { AddressBookEditGeneralModal } from '../address-book-edit-general';
+
+describe('AddressBookEditGeneralModal', () => {
+	it('should render an empty modal title when the address book does not exist', () => {
+		setupTest(
+			<AddressBookEditGeneralModal
+				addressBookId="non-existing-id"
+				onClose={jest.fn()}
+				onAddShare={jest.fn()}
+				onEditShare={jest.fn()}
+				onRevokeShare={jest.fn()}
+			/>
+		);
+
+		const modalTitle = screen.getByText(/Edit 's properties/i);
+		expect(modalTitle).toBeVisible();
+	});
+});
