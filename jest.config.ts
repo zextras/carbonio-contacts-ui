d0a9cc9970a7e2eadd1d2f7974c3e788b6313f77
Giovanni De Facci <giovanni.defacci@zextras.com>
2025-06-09 10:14:13 +0200

    fix: non relative path imports

--- jest.config.ts
+++ jest.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
 		enableGlobally: true
 	},
 	maxWorkers: '50%',
-	moduleDirectories: ['node_modules', 'utils'],
+	moduleDirectories: ['node_modules', 'utils', 'src'],
 	moduleNameMapper: {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/__mocks__/fileMock.js',
