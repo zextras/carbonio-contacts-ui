15a93cecb0def9187f17a7311be5f41f2fea8dee
Giuliano <75381134+giuliano176@users.noreply.github.com>
2025-07-07 10:10:33 +0200

    [CO-2194] UI breaks in search module (#361)

--- src/legacy/views/search/tests/search-view.test.tsx
+++ src/legacy/views/search/tests/search-view.test.tsx
@@ -15,17 +15,6 @@ import { FOLDERS, useFolderStore } from '@zextras/carbonio-ui-commons';
 import { noop } from 'lodash';
 import { HttpResponse } from 'msw';
 
-import { TIMERS } from 'constants/tests';
-import { CnItem } from 'network/api/types';
-import { createSoapContact, createSoapContactGroupV2 } from 'tests/utils';
-import {
-	SearchContactsRequest,
-	SearchContactsSoapRequest,
-	SearchContactsSoapResponse
-} from 'types';
-import { type SoapContact } from 'legacy/types/soap';
-import { Query } from 'legacy/views/search/search-types';
-import SearchView from 'legacy/views/search/search-view';
 import { makeListItemsVisible, screen, setupTest, triggerLoadMore } from '@test-setup';
 import { generateFolder } from '@test-utils/folders/folders-generator';
 import {
@@ -34,6 +23,17 @@ import {
 } from '@test-utils/network/msw/create-api-interceptor';
 import { generateSettings } from '@test-utils/settings/settings-generator';
 import { populateFoldersStore } from '@test-utils/store/folders';
+import { TIMERS } from 'constants/tests';
+import { type SoapContact } from 'legacy/types/soap';
+import { Query } from 'legacy/views/search/search-types';
+import SearchView from 'legacy/views/search/search-view';
+import { CnItem } from 'network/api/types';
+import { createSoapContact, createSoapContactGroupV2 } from 'tests/utils';
+import {
+	SearchContactsRequest,
+	SearchContactsSoapRequest,
+	SearchContactsSoapResponse
+} from 'types';
 
 const useMockedUseQuery = (): ReturnType<typeof useQuery> => {
 	const queryChip: QueryChip = {
